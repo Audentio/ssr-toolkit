1f4a3acab95d6da6747ba7bf09bc3171e3189f6c
Tushar Singh <tusharsingh@gmx.com>
2018-10-28 18:28:47 +0530

    setup window, catch render errors

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,7 @@ import sourcemap from 'source-map-support';
 import createExpressApp from './createExpressApp';
 import errorHandler from './errorHandler';
 import { SSRConfig } from './config.type';
+import setupBrowserGlobals from './utils/setupBrowserGlobals';
 
 // FormData support on server
 (global as any).FormData = FormData;
@@ -16,16 +17,22 @@ export default function createServer(config: SSRConfig) {
     const { after, renderApp, port = 8080 } = config;
 
     const app = createExpressApp(config);
+    
+    app.get('*', async (req, res, next) => {
+        try {
+            setupBrowserGlobals(req);
 
-    app.get('*', async (req, res) => {
-        const { html, context = {} } = await renderApp(req, res);
-        const { status = 200, action } = context;
+            const { html, context = {} } = await renderApp(req, res);
+            const { status = 200, action } = context;
 
-        if (action === 'REPLACE') {
-            // Redirect
-            res.redirect(status, context.url);
-        } else {
-            res.status(status).send(html);
+            if (action === 'REPLACE') {
+                // Redirect
+                res.redirect(status, context.url);
+            } else {
+                res.status(status).send(html);
+            }
+        } catch (error) {
+            next(error);
         }
     });
 
